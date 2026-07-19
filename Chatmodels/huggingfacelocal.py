5bf5a6cdd2b5505d6d22aa3380b2e263eeaf4f3e
sohamk256 <sohamkale33@gmail.com>
2026-07-19 23:17:34 +0530

    Remove venv from tracking and add .gitignore

--- Chatmodels/huggingfacelocal.py
+++ Chatmodels/huggingfacelocal.py
@@ -1,4 +1,13 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 from dotenv import load_dotenv
 load_dotenv()
 
+llm=  HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs ={"temperature":0.7, "max_new_tokens":512}
+)
+
+model = ChatHuggingFace(llm=llm)
+
+model.invoke("What is the capital of India?")
